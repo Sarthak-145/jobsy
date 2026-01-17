bd0afdba2b6dec573700f486bf4d2ab00bb7c197
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-17 17:33:26 +0530

    remove middlware testing in server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,8 @@ dotenv.config();
 
 import { connectDB } from './src/db/connect.js';
 import app from './src/app.js';
-import authenticate from './src/middlewares/authMiddleware.js';
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/protect', authenticate, (req, res) => {
-  res.status(200).json({ message: 'You are authorized' });
-});
 
 const start = async () => {
   await connectDB();
