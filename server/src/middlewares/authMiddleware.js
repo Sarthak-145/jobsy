6829f8b42cca5a913e15a78e377efadeafe9e239
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-17 17:18:46 +0530

    write requireRole api in middleware

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -19,4 +19,13 @@ const authenticate = (req, res, next) => {
   });
 };
 
+export const requireRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+    next();
+  };
+};
+
 export default authenticate;
